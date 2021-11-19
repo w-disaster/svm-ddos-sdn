ad0b0c45ee7e8c35b5e19c5a881e8c4ffc14e7c9
w-disaster <luca.fabri1999@gmail.com>
2021-11-19 11:54:50 +0100

    removed ryu.py

---
+++ network/controller/ryu.py
@@ -0,0 +1,33 @@
+from ryu.base import app_manager
+import threading
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
+
+class L2Switch(app_manager.RyuApp):
+    def __init__(self, *args, **kwargs):
+        t = threading.Thread(target=super(L2Switch, self).__init__, args=args,
+                kwargs=kwargs)
+        t.start()
+        #t.join()
+        #super(L2Switch, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+        ofp_parser = dp.ofproto_parser
+
+        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
+
+        data = None
+        if msg.buffer_id == ofp.OFP_NO_BUFFER:
+             data = msg.data
+
+        out = ofp_parser.OFPPacketOut(
+            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions, data = data)
+        dp.send_msg(out)
+
