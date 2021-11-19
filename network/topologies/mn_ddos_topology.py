3e9821df05b199ac5ddf434ae224beb57fe499a4
w-disaster <luca.fabri1999@gmail.com>
2021-11-19 11:38:09 +0100

    update traffic scripts

--- network/topologies/mn_ddos_topology.py
+++ network/topologies/mn_ddos_topology.py
@@ -1,52 +1,23 @@
-from mininet.net import Mininet
 from mininet.topo import Topo
-from mininet.cli import CLI
-from mininet.node import RemoteController
 
-class DDOSTopo(Topo):
-    "Simple topology example."
+N_HOSTS = 25
+DPID = "1"
+TARGET_IP = "137.204.10.100"
+
+class DDoSTopo(Topo):
+    "DDoS experimentation topology"
     
-    def __init__(self, **opts):
-        # Initialize topology and default optioe
-        Topo.__init__(self, **opts)
+    def build(self):
+        # Add one switch
+        s0 = self.addSwitch("s0", dpid=DPID)
 
-        s0 = self.addSwitch("s0", dpid="1")
-
-        for i in range(1, 6):
-            s = self.addSwitch("s" + str(i), dpid=str(i + 1))
-            for k in range(1, 6):
-                h = self.addHost('h' + str(i) + str(k), ip='137.204.' + str(i * 10) + 
-                    '.' + str(k * 10) + '/16') 
-                self.addLink(h, s, bw=10, delay="10ms")
-            self.addLink(s, s0, bw=10, delay="10ms")
-
+        # Add hosts and connect them to the switch
+        for i in range(1, N_HOSTS + 1):
+            h = self.addHost("h" + str(i), ip="137.204.10." + str(i + 1) + "/24") 
+            self.addLink(h, s0, bw=10, delay="10ms")
+           
         # Add target host
-        h_target = self.addHost('h_target', ip='137.204.60.10/16')
+        h_target = self.addHost("h_target", ip=TARGET_IP + "/24")
         self.addLink(h_target, s0, bw=10, delay="10ms")
 
- 
-if __name__ == '__main__':
-    ddostopo = DDOSTopo()
-    #topos = { 'mytopo': ( lambda: MyTopo() ) }
-    net = Mininet(topo=ddostopo,
-            controller=None)
-    
-    net.addController("c0",
-                      controller=RemoteController,
-                      ip="127.0.0.1",
-                      port=6653)
-
-    net.addController("c1",
-                      controller=RemoteController,
-                      ip="127.0.0.1",
-                      port=6633)
-
-    net.start()
-
-    for h in net.hosts:
-        if h.name != "h_target":
-            h.cmd("bash ./traffic.sh 137.204.60.10  &")
- 
-
-    CLI(net)
-    net.stop()
+topos = { 'ddostopo': ( lambda: DDoSTopo() ) }
--- network/topologies/traffic_peak.sh
+++ network/topologies/traffic_peak.sh
@@ -10,7 +10,7 @@ else
 	    N_PACKETS=$(shuf -i 10-20 -n 1)
 	    N_BYTES=$(shuf -i 150-200 -n 1)
 	    PAUSE=$(shuf -i 1-10 -n 1)
-	    sudo hping3 -c $N_PACKETS -d $N_BYTES --icmp "$TARGET_IP"
+	    sudo hping3 -c $N_PACKETS -d $N_BYTES --icmp --fast "$TARGET_IP"
 	    sleep $PAUSE
 	done
 fi
